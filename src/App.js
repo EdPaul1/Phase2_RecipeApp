2de60a652d1ea443258fdd872a69a0ceba5e0551
teddy5456 <126576621+teddy5456@users.noreply.github.com>
2023-05-03 12:53:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import RecipeList from './RecipeList';
-import recipesData from './recipes.json';
+import RecipeSearch from './RecipeSearch';
 
 function App() {
-  const [recipes, setRecipes] = useState(recipesData);
+  
 
   return (
     <div>
       <header>
-        <h1>Recipe App</h1>
-        <div className="search">
-          <input type="text" placeholder="Search for recipes" />
-          <button type="submit">Search</button>
-        </div>
+        <h1>Recipe App</h1>  
       </header>
-      <div className="container">
-        <div className="add-recipe">
-          <button>Add Recipe</button>
-        </div>
-        <RecipeList recipes={recipes} />
-      </div>
+      <RecipeSearch/>
+      <RecipeList/>
     </div>
   );
 }
