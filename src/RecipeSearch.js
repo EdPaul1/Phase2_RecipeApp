1802707c42c2bd0d9123d2e5de130b717b2ab72c
Teddy <teddywambua5456@gmail.com>
2023-05-03 13:06:02 +0300

    addedsearch

--- src/RecipeSearch.js
+++ src/RecipeSearch.js
@@ -1,30 +1,29 @@
 import React, { useState, useEffect } from "react";
-
 function RecipeSearch () {
     const [meals, setMeals] = useState([]);
     const [searchInputText, setSearchInputText] = useState('');
-
-    useEffect(() => {
+    const handleSearch = () => {
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
             .then(response => response.json())
             .then(data => setMeals(data.meals))
             .catch(error => console.error(error));
-    }, [searchInputText]);
-
-    const handleSearch = () => {
-        setSearchInputText(document.querySelector('.search input').value);
     }
-
+    const handleInputChange = (event) => {
+        setSearchInputText(event.target.value);
+    }
     return(
         <section>
             <div className="search">
-                <input type="text" placeholder="Search for recipes" onChange={handleSearch} />
+                <input type="text" placeholder="Search for recipes" value={searchInputText} onChange={handleInputChange} />
                 <button type="submit" onClick={handleSearch}>Search</button>
             </div>
             <div className="container">
-                <div className="add-recipe">
-                    <button>Add Recipe</button>
-                </div>
+                {meals.map(meal => (
+                    <div key={meal.idMeal}>
+                        <h2>{meal.strMeal}</h2>
+                        <img src={meal.strMealThumb} alt={meal.strMeal} />
+                    </div>
+                ))}
             </div>
         </section>
     )
