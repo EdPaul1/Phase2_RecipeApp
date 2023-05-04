a55ec1d8c20cab048debad524043804bae12cac6
Noah <noahmwangi@gmail.com>
2023-05-05 02:05:26 +0300

    Before Stash

---
+++ src/FavoriteButton.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+function FavoriteButton({ itemId }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  // Load the favorite state from the JSON file on component mount
+  useEffect(() => {
+    fetch('/favorites.json')
+      .then(response => response.json())
+      .then(favorites => setIsFavorite(favorites.includes(itemId)))
+      .catch(error => console.error(error));
+  }, [itemId]);
+
+  // Save the favorite state to the JSON file when it changes
+  useEffect(() => {
+    fetch('/favorites.json')
+      .then(response => response.json())
+      .then(favorites => {
+        if (isFavorite && !favorites.includes(itemId)) {
+          favorites.push(itemId);
+          return fetch('/favorites.json', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(favorites)
+          });
+        } else if (!isFavorite && favorites.includes(itemId)) {
+          favorites = favorites.filter(id => id !== itemId);
+          return fetch('/favorites.json', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(favorites)
+          });
+        }
+      })
+      .catch(error => console.error(error));
+  }, [itemId, isFavorite]);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <div onClick={toggleFavorite}>
+      <span>{isFavorite ? '❤️' : '♡'}</span>
+    </div>
+  );
+}
+
+export default FavoriteButton;
---
+++ src/images/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css'; // Import your CSS file here
+
+function Menu() {
+  return (
+    <div className="menu-container">
+      <img src="https://www.themealdb.com/images/media/meals/020z181619788503.jpg" alt="Menu item 1" />
+      <img src="https://www.themealdb.com/images/media/meals/41cxjh1683207682.jpg" alt="Meat dishes" />
+      <img src="https://www.themealdb.com/images/media/meals/usywpp1511189717.jpg" alt="Menu item 3" />
+      <img src="https://www.themealdb.com/images/media/meals/r33cud1576791081.jpg" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/media/meals/sxysrt1468240488.jpg" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/category/side.png" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/category/pork.png" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/category/seafood.png" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/category/goat.png" alt="Menu item 4" />
+      <img src="https://www.themealdb.com/images/category/breakfast.png" alt="Menu item 4" />
+      
+    </div>
+  );
+}
+
+export default Menu;
