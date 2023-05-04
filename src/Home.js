d7b20593d713e1f5582599835f0a2ba6355ca4de
Noah <noahmwangi@gmail.com>
2023-05-04 22:40:37 +0300

    creating Home Page

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,29 @@
 
 import React from 'react';
+import "./index.css";
   
-const Home = () => {
+function Home() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Annual Report</h1>
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>
+        This is the homepage of our React app. Here you can find information
+        about our app and what it does.
+      </p>
+      <p>
+        To get started, you can navigate to the various pages using the menu
+        above or by clicking the links below.
+      </p>
+      <ul>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/contact">Contact Us</a>
+        </li>
+      </ul>
     </div>
   );
-};
-  
+}
+
 export default Home;
