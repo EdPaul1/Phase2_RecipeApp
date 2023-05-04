e2f1103609b1941b053d65a589401569c0e49840
Edwin Paul <saidarajab101@gmail.com>
2023-05-04 22:10:26 +0300

    Navbar Element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import RecipeList from './RecipeList';
 import RecipeSearch from './RecipeSearch';
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Components/Navbar";
+import Home from "./Home";
+import About from "./About";
+import Favorites from "./Favorites"
+import News from './News';
+// import index from './Components/Navbar/index';
 
 function App() {
   
 
   return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/favorites' component={Favorites} />
+         <Route path='/news' component={News} />
+      </Routes>
+    </Router>
     <div>
       <header>
         <h1 className='title'>Recipe App</h1>  
@@ -14,6 +31,7 @@ function App() {
       <RecipeSearch/>
       <RecipeList/>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,39 @@
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+}  from './NavbarElements';
+import Home from "../../Home";
+import About from "../../About";
+import Favorites from "../../Favorites"
+import News from '../../News';
+  
+const Navbar = () => {
+ return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/home' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/favorites' activeStyle>
+           Favorites
+          </NavLink>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/news' activeStyle>
+            News
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
