b5a457f4d40cfbdd668581ad3c3e03523d631896
Noah <noahmwangi@gmail.com>
2023-05-05 10:04:24 +0300

    FavoritesButton

--- src/FavoriteButton.js
+++ src/FavoriteButton.js
@@ -1,48 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function FavoriteButton({ itemId }) {
   const [isFavorite, setIsFavorite] = useState(false);
 
-  // Load the favorite state from the JSON file on component mount
   useEffect(() => {
-    fetch('/favorites.json')
-      .then(response => response.json())
-      .then(favorites => setIsFavorite(favorites.includes(itemId)))
-      .catch(error => console.error(error));
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setIsFavorite(favorites.includes(itemId));
   }, [itemId]);
 
-  // Save the favorite state to the JSON file when it changes
-  useEffect(() => {
-    fetch('/favorites.json')
-      .then(response => response.json())
-      .then(favorites => {
-        if (isFavorite && !favorites.includes(itemId)) {
-          favorites.push(itemId);
-          return fetch('/favorites.json', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(favorites)
-          });
-        } else if (!isFavorite && favorites.includes(itemId)) {
-          favorites = favorites.filter(id => id !== itemId);
-          return fetch('http://localhost:8001/favorites', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(favorites)
-          });
-        }
-      })
-      .catch(error => console.error(error));
-  }, [itemId, isFavorite]);
-
-  const toggleFavorite = () => {
+  const handleToggleFavorite = () => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const updatedFavorites = isFavorite
+      ? favorites.filter((id) => id !== itemId)
+      : [...favorites, itemId];
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
     setIsFavorite(!isFavorite);
   };
 
   return (
-    <div onClick={toggleFavorite}>
-      <span>{isFavorite ? '❤️' : '♡'}</span>
-    </div>
+    <button onClick={handleToggleFavorite}>
+      {isFavorite ? "Remove from favorites" : "Add to favorites"}
+    </button>
   );
 }
 
